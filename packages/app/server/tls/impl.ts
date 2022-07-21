AmadeusITGroup/kassette

commit 1ec84e5708b00b66db1afb4eb6834c62b4d081c0

feat: add tlsKeySize option and change default value to 2048

packages/app/server/tls/impl.ts
@@ -11,7 +11,13 @@ export class TLSManager {
   private _caPem: string;
   private _contexts = new Map<string, Promise<SecureContext>>();
 
-  constructor(private _config: { root: string; tlsCAKeyPath: string | null }) {}
+  constructor(
+    private _config: {
+      root: string;
+      tlsCAKeyPath: string | null;
+      tlsKeySize: number;
+    },
+  ) {}
 
   public async init() {
     const { root, tlsCAKeyPath } = this._config;
@@ -52,7 +58,9 @@ export class TLSManager {
       }
     }
     if (!this._caObject) {
-      const ca = await createCertificate(['DO NOT TRUST kassette TLS interception certificate']);
+      const ca = await createCertificate(['DO NOT TRUST kassette TLS interception certificate'], {
+        keySize: this._config.tlsKeySize,
+      });
       this._caObject = ca.object;
       this._caPem = ca.cert;
       if (tlsCAFilePath) {
@@ -65,7 +73,11 @@ export class TLSManager {
     let res = this._contexts.get(host);
     if (!res) {
       res = (async () => {
-        const certificate = await createCertificate([host], this._caObject, false);
+        const certificate = await createCertificate([host], {
+          issuer: this._caObject,
+          ca: false,
+          keySize: this._config.tlsKeySize,
+        });
         return createSecureContext({
           cert: `${certificate.cert}\n${this._caPem}`,
           key: certificate.key,

packages/app/server/impl.ts
@@ -71,7 +71,11 @@ export async function spawnServer({ configuration, root }: ApplicationData): Pro
     await mock.process();
   });
 
-  const tlsManager = new TLSManager({ tlsCAKeyPath: configuration.tlsCAKeyPath.value, root });
+  const tlsManager = new TLSManager({
+    tlsCAKeyPath: configuration.tlsCAKeyPath.value,
+    tlsKeySize: configuration.tlsKeySize.value,
+    root,
+  });
   await tlsManager.init();
 
   const handleSocket = (socket: Socket) => {

packages/app/configuration/index.ts
@@ -96,6 +96,12 @@ export async function getConfiguration({
       apiValue: apiConfiguration.tlsCAKeyPath,
       defaultValue: null,
     }),
+    tlsKeySize: buildProperty({
+      cliValue: cliConfiguration.tlsKeySize,
+      fileValue: fileConfiguration.tlsKeySize,
+      apiValue: apiConfiguration.tlsKeySize,
+      defaultValue: 2048,
+    }),
     delay: buildProperty({
       cliValue: cliConfiguration.delay,
       fileValue: fileConfiguration.delay,

packages/app/configuration/model.ts
@@ -186,6 +186,11 @@ export interface CLIConfigurationSpec {
    * through kassette.
    */
   readonly tlsCAKeyPath?: string | null;
+
+  /**
+   * Size in bits of generated RSA keys.
+   */
+  readonly tlsKeySize?: number;
 }
 
 /**

packages/app/server/configuration/index.spec.ts
@@ -91,6 +91,7 @@ describe('server configuration', () => {
           hostname: { value: '127.0.0.1', origin: 'default' },
           port: { value: 8080, origin: 'cli' },
           tlsCAKeyPath: { value: 'dummy/tls/path', origin: 'cli' },
+          tlsKeySize: { value: 1024, origin: 'cli' },
           proxyConnectMode: { value: 'intercept', origin: 'default' },
           onProxyConnect: { value: nop, origin: 'file' },
           remoteURL: { value: 'http://dummy.com:5555', origin: 'file' },
@@ -118,6 +119,7 @@ describe('server configuration', () => {
 - (${highlighted('cli')} | file | api | default) CA key file path: ${highlighted(
         '"dummy/tls/path"',
       )}
+- (${highlighted('cli')} | file | api | default) TLS key size: ${highlighted('1024')}
 - (cli | ${highlighted('file')} | api | default) mocks folder: ${highlighted('"dummy/relative"')}
 - (cli | file | api | ${highlighted('default')}) delay: ${highlighted('"recorded"')}
 - (cli | ${highlighted('file')} | api | default) custom request handler: ${highlighted('✓')}
@@ -140,6 +142,7 @@ Root folder used for relative paths resolution: ${highlighted('C:/dummy/root/fol
           hostname: { value: '127.0.0.2', origin: 'cli' },
           port: { value: 8080, origin: 'cli' },
           tlsCAKeyPath: { value: null, origin: 'default' },
+          tlsKeySize: { value: 2048, origin: 'default' },
           proxyConnectMode: { value: 'forward', origin: 'cli' },
           onProxyConnect: { value: nop, origin: 'api' },
           remoteURL: { value: 'http://dummy.com:5555', origin: 'file' },
@@ -165,6 +168,7 @@ Root folder used for relative paths resolution: ${highlighted('C:/dummy/root/fol
 - (cli | file | api | ${highlighted('default')}) proxy mode: ${highlighted('"remote"')}
 - (${highlighted('cli')} | file | api | default) proxy connect mode: ${highlighted('"forward"')}
 - (cli | file | api | ${highlighted('default')}) CA key file path: ${highlighted('null')}
+- (cli | file | api | ${highlighted('default')}) TLS key size: ${highlighted('2048')}
 - (cli | ${highlighted('file')} | api | default) mocks folder: ${highlighted('"dummy/relative"')}
 - (cli | file | api | ${highlighted('default')}) delay: ${highlighted('"recorded"')}
 - (cli | ${highlighted('file')} | api | default) custom request handler: ${highlighted('✓')}

packages/app/server/configuration/index.ts
@@ -73,6 +73,7 @@ export function logApplicationData({ configuration, root }: ApplicationData) {
   logProperty('proxy mode', configuration.mode);
   logProperty('proxy connect mode', configuration.proxyConnectMode);
   logProperty('CA key file path', configuration.tlsCAKeyPath);
+  logProperty('TLS key size', configuration.tlsKeySize);
   logProperty('mocks folder', configuration.mocksFolder);
   logProperty('delay', configuration.delay);
   logProperty('custom request handler', configuration.hook, true);

packages/app/server/tls/certs.ts
@@ -2,8 +2,17 @@ import { pki, md } from 'node-forge';
 
 const TEN_YEARS_IN_MS = 10 * 365 * 24 * 60 * 60 * 1000;
 
-export async function createCertificate(hostNames: string[], issuer?: pki.Certificate, ca = true) {
-  const keyPair = await pki.rsa.generateKeyPair(1024);
+interface CertificateOptions {
+  issuer?: pki.Certificate;
+  ca?: boolean;
+  keySize: number;
+}
+
+export async function createCertificate(
+  hostNames: string[],
+  { issuer, ca, keySize }: CertificateOptions,
+) {
+  const keyPair = await pki.rsa.generateKeyPair(keySize);
   const cert = pki.createCertificate();
   const now = Date.now();
   cert.serialNumber = `${now}`;

packages/cli/index.ts
@@ -83,6 +83,10 @@ const { version } = require('../../package.json');
       describe: 'path to a PEM-encoded CA certificate and key file, created if it does not exist.',
       string: true,
     })
+    .option('tls-key-size', {
+      describe: 'size in bits of generated RSA keys.',
+      type: 'number',
+    })
     .option('d', {
       alias: ['delay'],
       describe: 'mock response artificial delay',
@@ -105,6 +109,7 @@ const { version } = require('../../package.json');
       mocksFolder: options.f,
       skipLog: options.q,
       tlsCAKeyPath: options.k,
+      tlsKeySize: options.tlsKeySize,
     },
     configurationPath: options.c as string,
   });

commit 80a8ca6b9f2ef8d51b69633aad058c89ca691ea1

fix: missing CA option when generating CA certificate
This commit fixes a regression introduced in 1ec84e5708b00b66db1afb4eb6834c62b4d081c0
ca, which was the last parameter of the createCertificate function, was true
by default before that commit, but the default value was lost when
refactoring the parameters of createCertificate to accept an object.

packages/app/server/tls/impl.ts
@@ -60,6 +60,7 @@ export class TLSManager {
     if (!this._caObject) {
       const ca = await createCertificate(['DO NOT TRUST kassette TLS interception certificate'], {
         keySize: this._config.tlsKeySize,
+        ca: true,
       });
       this._caObject = ca.object;
       this._caPem = ca.cert;
@@ -75,7 +76,6 @@ export class TLSManager {
       res = (async () => {
         const certificate = await createCertificate([host], {
           issuer: this._caObject,
-          ca: false,
           keySize: this._config.tlsKeySize,
         });
         return createSecureContext({