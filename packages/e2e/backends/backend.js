eec60e5f81457219d743006b94058c7afb794eb1
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2025-12-10 14:33:55 +0100

    chore: remove koa

--- packages/e2e/backends/backend.js
+++ packages/e2e/backends/backend.js
@@ -19,7 +19,8 @@ function Launcher({ processName, hookName, secure, http2Server }) {
           onExit,
           secure,
           http2Server,
-          registerRoutes: (router) => registerRoutes({ router, console, pushResult, hookName }),
+          onRequest: (request, response) =>
+            onRequest({ request, response, console, pushResult, hookName }),
         });
       },
     });
@@ -27,18 +28,31 @@ function Launcher({ processName, hookName, secure, http2Server }) {
 }
 exports.Launcher = Launcher;
 
-function registerRoutes({ router, console, pushResult, hookName }) {
-  router.all('use case implementation', '/:pathname*', async function (context) {
-    const { pathname } = context.params;
+/**
+ * @param {{request: import('http').IncomingMessage, response: import('http').ServerResponse}} param
+ */
+async function onRequest({ request, response, console, pushResult, hookName }) {
+  const { pathname } = new URL(request.url, `http://127.0.0.1/`);
 
-    const { useCase, name, iteration } = getCurrentContext();
-    console.log(`Executing use case "${name}", iteration ${iteration}`);
+  const { useCase, name, iteration } = getCurrentContext();
+  console.log(`Executing use case "${name}", iteration ${iteration}`);
 
-    context.response.status = 200;
-    let result = {};
-    if (useCase[hookName] != null) {
-      result = await useCase[hookName](context, { console, useCase, name, iteration, pathname });
+  let result = {};
+  response.statusCode = 200;
+  if (useCase[hookName] != null) {
+    try {
+      result = await useCase[hookName](
+        { request, response },
+        { console, useCase, name, iteration, pathname },
+      );
+    } catch (error) {
+      console.error(`Error in ${name} ${hookName}:`, error);
+      response.statusCode = 500;
+      response.end('Internal Server Error');
     }
-    await pushResult({ useCase: name, iteration, data: result });
-  });
+  }
+  if (!response.writableEnded) {
+    response.end();
+  }
+  await pushResult({ useCase: name, iteration, data: result });
 }
